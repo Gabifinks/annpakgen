a9e54336e8c470c4c35c6651ed459f4ade292044
Jeniffer Fonseca <79013581+Jenifferbio@users.noreply.github.com>
2025-11-30 21:29:11 -0300

    feat: added find_genome variable

--- src/extraction_strategy/gene_extraction_strategy.py
+++ src/extraction_strategy/gene_extraction_strategy.py
@@ -16,14 +16,7 @@ class GeneExtractionStrategy(ExtractionStrategy):
         else:
             print("Gene not found")
             return []
-        
-    def extract_gene(self, genbank_lines: List[str]) -> List[str]:
-        for line in genbank_lines:
-            if line.startswith("ORIGIN"):
-                origin_index = genbank_lines.index(line) + 6
-                end_index = genbank_lines.index("//")
-                sequence_box = genbank_lines[origin_index:end_index]
-    def extract_coordinates(self, genbank_lines: List[str], gene: str = "rsmI") -> List[str]:
+    def find_coordinates(self, genbank_lines: List[str], gene: str = "rsmI") -> List[str]:
         coordinates = []
         for i, line in enumerate(genbank_lines):
             """As coordenadas estão na linha anterior à linha que contém /gene="""
@@ -33,6 +26,30 @@ class GeneExtractionStrategy(ExtractionStrategy):
                 coord = re.search(r'(\d+\.\.\d+)', gene_coord)
                 if coord:
                     coordinates.append(coord.group(1))
-        return coordinates                   
+        return coordinates
+        
+    def find_genome(self, genbank_lines: List[str]) -> List[str]:
+        """Extrai a sequência do genoma do bloco entre 'ORIGIN' e '//'."""
+        origin_index = None
+        end_index = None
+    
+        for i, line in enumerate(genbank_lines):
+            if line.strip().upper().startswith("ORIGIN"):
+                origin_index = i + 1
+                break
+        if origin_index is None:
+            return []
+    
+        for i in range(origin_index, len(genbank_lines)):
+            if genbank_lines[i].strip().startswith("//"):
+                end_index = i
+                break
+        if end_index is None:
+            return []
+    
+        sequence_box = genbank_lines[origin_index:end_index]
+        return sequence_box
+                              
     def extract_sequence(self, genbank_lines: List[str], coordinates: str) -> str:
-        sequence = """Tenho que extrair a sequência com base nas coordenadas fornecidas anteriormente"""
+        """Tenho que extrair a sequência com base nas coordenadas fornecidas anteriormente"""
+        
